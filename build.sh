3dbe6e8890c91c35cb79eff80f3b2f222bb81d89
salamer <salamer_gaga@163.com>
2024-09-14 19:21:51 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 apt-get update && \
-    apt-get install -y libevent
+    apt-get install -y sudo libevent-dev
 pip install playwright && \
     playwright install --with-deps
+playwright install-deps
 pip install flask gunicorn
